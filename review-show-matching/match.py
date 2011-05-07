95fedc1372a0b563db270926c28068ebef56065a
Bruno Panara <bruno.panara@gmail.com>
2011-05-07 10:14:33 +0100

    added publishing date to threeweeks. working on fuzzy matching between reviews and shows

--- review-show-matching/match.py
+++ review-show-matching/match.py
@@ -1,4 +1,7 @@
 import simplejson
+import string
+
+from BeautifulSoup import BeautifulStoneSoup
 
 matches = []
 
@@ -10,20 +13,45 @@ shows_data= open('listings.json')
 shows = simplejson.load(shows_data)
 shows_data.close()
 
-print "We have %d reviews for %d shows." % (len(reviews), len(shows))
-
 show_names = []
+failures = []
+
 for show in shows:
-    show_names.append((show['id'], show['event_desc']))
+    if show['area_code'] == "EFF":
+        show_names.append((show['id'], show['event_desc']))
+
+print "We have %d reviews for %d shows." % (len(reviews), len(show_names))
 
 for review in reviews:
+    success = 0
+
     name = review['review']['name']
+    name = unicode(BeautifulStoneSoup(name,convertEntities=BeautifulStoneSoup.HTML_ENTITIES ))
+    for c in string.punctuation:
+        name = name.replace(c,"")
+    name = string.lower(name) 
+
     for show in show_names:
-        if show[1] == name:
-            matches.append((show[0],name))
-            #print "("+show[1]+", "+name+")"
-            continue
+        show_name = show[1]
+        for c in string.punctuation:
+            show_name = show_name.replace(c,"")
+        show_name = string.lower(show_name) 
+        show_name = unicode(BeautifulStoneSoup(show_name,convertEntities=BeautifulStoneSoup.HTML_ENTITIES ))
+
+        if not [word for word in name.split(' ') if word not in show_name.split(' ')]:
+            matches.append((show[0], review['review']['name'])
+            success = 1
+            #if show[1] != review['review']['name']:
+            #    print "("+show[1]+", "+review['review']['name']+")"
+            break
+        else:
+            failures.append(review['review']['name'])
+
+    #if not success:
+    #    print review['review']['name']
 
 print "%d reviews were matched" % (len(matches))
+print "%d reviews were not matched" % (len(failures))
 
+print failures
 
