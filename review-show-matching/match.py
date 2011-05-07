c8cceea470c8bf78fe0a2892fe43dcd0b3a35ee3
Bruno Panara <bruno.panara@gmail.com>
2011-05-07 11:18:15 +0100

    done with finding matches for fest.json, moving on to other corpora

--- review-show-matching/match.py
+++ review-show-matching/match.py
@@ -1,5 +1,6 @@
 import simplejson
 import string
+import difflib
 
 from BeautifulSoup import BeautifulStoneSoup
 
@@ -14,7 +15,7 @@ shows = simplejson.load(shows_data)
 shows_data.close()
 
 show_names = []
-failures = []
+#failures = []
 
 for show in shows:
     if show['area_code'] == "EFF":
@@ -26,32 +27,47 @@ for review in reviews:
     success = 0
 
     name = review['review']['name']
-    name = unicode(BeautifulStoneSoup(name,convertEntities=BeautifulStoneSoup.HTML_ENTITIES ))
+    #name = unicode(BeautifulStoneSoup(name,convertEntities=BeautifulStoneSoup.HTML_ENTITIES ))
     for c in string.punctuation:
         name = name.replace(c,"")
     name = string.lower(name) 
 
+    hiscore = 0
+    bestmatch = ""
     for show in show_names:
+
         show_name = show[1]
         for c in string.punctuation:
             show_name = show_name.replace(c,"")
         show_name = string.lower(show_name) 
-        show_name = unicode(BeautifulStoneSoup(show_name,convertEntities=BeautifulStoneSoup.HTML_ENTITIES ))
+        #show_name = unicode(BeautifulStoneSoup(show_name,convertEntities=BeautifulStoneSoup.HTML_ENTITIES ))
 
-        if not [word for word in name.split(' ') if word not in show_name.split(' ')]:
-            matches.append((show[0], review['review']['name'])
+        ratio = difflib.SequenceMatcher(None, name, show_name).ratio()
+        if not [word for word in name.split(' ') if word not in show_name.split(' ')] or ratio > 0.80 or name in show_name:
+            matches.append((show[0], review['review']['name']))
             success = 1
+            #print ratio
             #if show[1] != review['review']['name']:
             #    print "("+show[1]+", "+review['review']['name']+")"
             break
         else:
-            failures.append(review['review']['name'])
+            if ratio >= hiscore:
+                hiscore = ratio
+                bestmatch = show_name
 
-    #if not success:
-    #    print review['review']['name']
+#    if not success:
+        #print name.split(' ')
+        #failures.append(review['review']['name'])
+#        if hiscore > 0.6:
+#            print name
+#            print bestmatch+" "+str(hiscore)
+
+f = open('fest_matches.txt', 'w')
+f.write(str(matches))
+f.close()
 
 print "%d reviews were matched" % (len(matches))
-print "%d reviews were not matched" % (len(failures))
+#print "%d reviews were not matched" % (len(failures))
 
-print failures
+#print failures
 
